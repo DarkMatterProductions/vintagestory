72755cb4435f34097c8714eda846935314880ec3
jboylan <>
2026-02-17 18:20:33 +0000

    parameters of 'Integrate Release' build configuration were updated (TeamCity change in 'Vintage Story Dedicated Server' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -239,6 +239,7 @@ object IntegrateRelease : BuildType({
         param("build.docker.tag", "")
         password("system.vcs.auth.token", "credentialsJSON:cd1076b1-4804-4dba-babf-6e3d93e09c68")
         text("build.pullrequest.id", "", label = "Pull Request ID#", description = "PR Id to Integrate into main branch", display = ParameterDisplay.PROMPT, allowEmpty = true)
+        password("env.SYSTEM_VCS_AUTH_TOKEN", "credentialsJSON:701e7549-b2b2-418e-8ba1-e7a644033fd7")
         param("build.version.old", "")
         param("build.docker.version.new", "")
     }
