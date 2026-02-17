b18a685301b4f418b24b7d40f9e8f97a620222c6
James Boylan <drahkar@darkmatter-productions.com>
2026-02-17 22:34:01 +0000

    'https://github.com/DarkMatterProductions/vintagestory#refs/heads/features/wildcard' VCS root was updated (TeamCity change in 'Vintage Story Dedicated Server' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2430,7 +2430,7 @@ object RunTestsAndLinting : BuildType({
 object HttpsGithubComDarkMatterProductionsVintagestoryRefsHeadsFeaturesWildcard : GitVcsRoot({
     name = "https://github.com/DarkMatterProductions/vintagestory#refs/heads/features/wildcard"
     url = "https://github.com/DarkMatterProductions/vintagestory"
-    branch = "refs/heads/feature/%teamcity.build.branch%"
+    branch = "refs/heads/feature/main"
     branchSpec = """
         +:refs/heads/feature/*
         +:refs/heads/fix/*
